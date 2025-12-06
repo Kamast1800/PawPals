4691ea1ea7180ada874279b0cde5179ba3b278f2
Kamast1800 <kamast@csuchico.edu>
2025-12-06 14:27:39 -0800

    I hate this

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-gray-900 text-white border-b border-gray-700 shadow-lg z-50">
+    <nav className="fixed bottom-0 left-0 right-0 bg-gray-900 text-white border-t border-gray-700 shadow-lg z-50">
       <div className="max-w-md mx-auto px-2">
         <div className="flex justify-between items-center h-16">
           {navItems.map((item) => (
